ce01ae24968593e32c5ac43ed70ee0d216bcd916
jjintokyo <jjintokyo@hotmail.com>
2023-09-30 17:00:55 +0200

    Add files via upload

--- NoPi/random_set.sh
+++ NoPi/random_set.sh
@@ -2,7 +2,7 @@
 
 read_xml_file () { IFS='>' read -d '<' -r lines; }
 
-if curl --fail http://dir.xiph.org/yp.xml -o /root/random/yp.xml; then
+if /usr/bin/curl --fail http://dir.xiph.org/yp.xml -o /root/random/yp.xml; then
 
     while read_xml_file; do
         tag="${lines%>*}"
@@ -10,6 +10,9 @@ if curl --fail http://dir.xiph.org/yp.xml -o /root/random/yp.xml; then
         if [ "$tag" == "listen_url" ] && [ "$value" != " " ] && [ ! -z "$value" ]; then echo "$value"; fi;
     done < /root/random/yp.xml > /root/random/yp.txt
 
-    head -n 999 /root/random/yp.txt >> /root/random/yp.txt
+    /usr/bin/head -n 999 /root/random/yp.txt >> /root/random/yp.txt
+    /usr/bin/rm -f /var/www/html/yp.db
     /root/random/random_set.py
+    /usr/bin/ln /root/random/yp.db /var/www/html/
+
 fi
