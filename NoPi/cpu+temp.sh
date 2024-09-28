d793dba7a4d2cfede9d9b5cc8eb81e835873bdf3
jjintokyo <jjintokyo@hotmail.com>
2024-09-28 22:03:23 +0200

    Add files via upload

--- NoPi/cpu+temp.sh
+++ NoPi/cpu+temp.sh
@@ -15,7 +15,7 @@ get_cpu_percent()     { TRY=3; LOOP=0; PREV_TOTAL=0; PREV_IDLE=0;
                         done; }
 get_in_temperature()  { IN_TEMP=$(/usr/bin/cat "/sys/class/thermal/thermal_zone0/temp"); ((IN_TEMP=IN_TEMP/1000)); }
 get_out_temperature() { OUT_TEMP=$(/usr/bin/curl -sf\
-   "http://api.openweathermap.org/data/2.5/weather?q=Publier,FR&units=metric&appid=d24d74df68aacf2d69495246af937bc6" |\
+   "http://api.openweathermap.org/data/2.5/weather?q=xyz" |\
                         /usr/bin/jq -r '.main.temp' | /usr/bin/cut -d'.' -f1); }
 
 get_cpu_percent; get_in_temperature; get_out_temperature;
